4b2be636758f789f5618df72990c3b067611612e
simfeld <sml.imfeld@gmail.com>
2014-11-30 18:57:11 +0100

    work done by vjunet on checkEngagements and makeMatch

--- code/checkEngagements.m
+++ code/checkEngagements.m
@@ -1,43 +1,87 @@
-function [ stable ] = checkEngagements( engaged, m, f )
+function [ stable,counter ] = checkEngagements2( engaged, m, f )
 %checkEngagements checks whether a set of engagements is stable
 %   dimensions must be correct, m=nxn, f=nxn, engaged=nx2
 %   men an women encoded as integers from 1 to n
-%   returns true for stable engagements, false otherwise
-invengaged = sortrows(engaged,2);
-invengaged = invengaged(:,[2 1])
-stable = true;
-n = size(m,1);
-for he=1:n
-    she = engaged(he,2);
-    if (she==0)
-        continue;
-    end
-    hisindex = find(f(she,:)==he,1)
-    herindex = find(m(he,:)==she,1)
-    helikesbetter = m(he,1:herindex)
-    shelikesbetter = f(she,1:hisindex)
-    %if (herindex~=1)
-        for i=1:size(helikesbetter)
-            girl = helikesbetter(i)
-            girlsguy = invengaged(girl,2)
-            girllikes = f(girl,:)
-            if (find(girllikes==he,1)<find(girllikes==girlsguy,1))
-                stable = false;
-                fprintf('man %d and woman %d like each other better\n', he, girl);
-            end
-        end
-    %end
-    %if (hisindex~=1)
-        for i=1:size(shelikesbetter)
-            guy = shelikesbetter(i);
-            guysgirl = engaged(guy,2);
-            guylikes = m(guy,:);
-            if (find(guylikes==she,1)<find(guylikes==guysgirl,1))
-                stable = false;
-                fprintf('man %d and woman %d like each other better\n', guy, she);
-            end
-        end
-    %end
-end
-end
+%   returns true for stable engagements, false otherwise; return "counter",
+%   the number of unstable mariages
 
+n = size(m,1);
+
+%invert the engaged matrix such that the neu matrix has the index of the
+%women on the column one and those of their respective husband in row two
+TransitionMatrix=zeros(n,2);
+invengaged=zeros(n,2);
+TransitionMatrix(:,1)=engaged(:,2);
+TransitionMatrix(:,2)=engaged(:,1);
+i=1;
+while i~=n+1
+    index=TransitionMatrix(i,1);
+    while index==0 && i~=n
+        i=i+1;
+        index=TransitionMatrix(i,1);
+    end %while
+    if index==0 && i==n
+        break;
+    end %if
+    invengaged(index,:)=TransitionMatrix(i,:);
+    i=i+1;
+end %while
+invengaged;
+stable=true;
+he=1;
+counter=0;
+while he<=n
+    she = engaged(he,2); %"she" is engaged to "he"
+    
+        while she==0 && he~=n   %"he" is not engaged, so there isn't unstability ==> check the next man
+            he = he+1;
+            she = engaged(he,2);
+        end %while
+        if she==0 % ==> "he"=n is not engaged ==> nothing to check.
+            break;
+        end %if
+            
+    hisindex = find(f(she,:)==he,1);
+    herindex = find(m(he,:)==she,1);
+    helikesbetter = m(he,1:herindex);
+    shelikesbetter = f(she,1:hisindex);
+    
+    if ~isempty(shelikesbetter) %there is no one on earth "she" likes better
+        for i=1:size(shelikesbetter) %Loop to check if there is unstability for the girl
+            guy = shelikesbetter(i); %all the guys "she" likes better
+            guysgirl = engaged(guy,2); %the guy "she" is engaged to           
+            if guysgirl == 0 && ~isempty(find(m(guy,:)== she,1)) %if this guy isn't engaged, then "she" could be with him ==> unstable, unless he doesn't know her.
+               stable = false;
+               counter=counter+1;
+               fprintf('man %d and woman %d like each other better\n', guy, she);
+            else
+                guylikes = m(guy,:);   %the ordered preferences of "guy"
+                if (find(guylikes==she,1)<find(guylikes==guysgirl,1)) %if "guy" also likes "she" better than his wife ==> unstable
+                    stable = false;
+                    counter=counter+1;
+                    fprintf('man %d and woman %d like each other better\n', guy, she);
+                end %if_3
+            end %if_2
+        end %for
+    end %if_1
+    
+    if ~isempty(shelikesbetter) %there is no one on earth "he" likes better
+        for i=1:size(helikesbetter) %Loop to check if there is unstability for the man
+            girl = helikesbetter(i); %all the girls "he" likes better
+            girlsguy = invengaged(girl,2); %the girl "he" is engaged to
+            if girlsguy == 0   %if this girl isn't engaged, then she" could be with her ==> unstable
+                stable=false;
+                fprintf('man %d and woman %d like each other better\n', he, girl);
+            else
+                girllikes = f(girl,:);   %the ordered preferences of "girl"
+                if (find(girllikes==he,1)<find(girllikes==girlsguy,1))  %if "guy" also likes "she" better than his wife ==> unstable
+                    stable = false;
+                    fprintf('man %d and woman %d like each other better\n', he, girl);
+                end %if_3
+            end %if_2
+        end %for
+    end %if_1
+    
+    he=he+1;  %go to the next man
+end %while
+end
--- code/makeMatch.m
+++ code/makeMatch.m
@@ -1,51 +1,83 @@
-function [ engaged ] = makeMatch( m, f )
+function [ engaged ] = makeMatch2( m, f )
 %makeMatch finds engagements for preferences according to Gale-Shapley algorithm
-%   dimensions must be correct, m=nxn, f=nxn
 %   men an women encoded as integers from 1 to n
+%   m ==> preference matrix of the men. Each row corresponds to a man and
+%   the elements are the women listed according to his preferences.
+%   f ==> preference matrix of the women. Each row corresponds to a woman and
+%   the elements are the men listed according to her preferences.
+%   Dimensions must be correct, m=nxn, f=nxn.
 %   result is nx2 Matrix containing matches
-disp('mens preferences:')
+disp('men´s preferences:')
 disp(m);
-disp('womens preferences:')
+disp('women´s preferences:')
 disp(f);
 n = size(m,1);
 n2 = size(f,1);
 assert(n == size(m,2));
 assert(n==n2);
-freemen = [(1:n)',ones(n,1)];
-engaged = zeros(n,2);%1=men, 2=women
+freemen = [(1:n)',ones(n,1)]; %column 1= men; column 2: 1==>man is free, 0==>man isn't free
+engaged = zeros(n,2);%column 1=men; column 2=women
+dumped=0;
 while ~isempty(find(freemen(:,2)==1,1))
-    theman = find(freemen(:,2)==1,1);
+    theman = find(freemen(:,2)==1,1); %the first man free on the list
     thegirl = m(theman,1);%his first choice
-    if (thegirl==0)
-        freemen(theman,2)=0;%has no partner
-        continue;
-    end
-    index = find(engaged(:,2)==thegirl,1);%index of possible fiance of his first choice 
-    if(isempty(index) )
-        engaged(theman,1) = theman;%make new engagement
-        engaged(theman,2) = thegirl;
-        fprintf('%d engaged to %d\n', theman, thegirl);
-        freemen(theman,2) = 0;%man is not free anymore
-    else
-        fiance = engaged(index,1);%fiance of first choice
-        girlprefers = f(thegirl,:);
-        if(find(girlprefers==theman,1)<find(girlprefers==fiance,1))
-            engaged(theman,1) = theman;%change fiance of the girl
-            engaged(theman,2) = thegirl;
-            engaged(fiance,1) = 0;%fiance is free again
-            engaged(fiance,2) = 0;
-            fprintf('%d dumped %d for %d\n', thegirl, fiance, theman);
-            freemen(theman,2) = 0;
-            freemen(fiance,2) = 1;
+        if thegirl==0; %"theman" doesn't know any free girls who want him, he'll be alone :(
+            freemen(theman,2)=0;
+            engaged(theman,:)=0;
         else
-            m(theman,:) = [m(theman,2:n) 0];%make preference list of the man smaller
-        end
-    end
-end
-stable = checkEngagements(engaged,m,f);%check the engagements
+            index = find(engaged(:,2)==thegirl,1);%index of possible fiance of his first choice 
+            if(isempty(index) ) %"thegirl" is free ==> "theman" will be engaged to "thegirl".
+                engaged(theman,1) = theman;%make new engagement
+                engaged(theman,2) = thegirl;
+                fprintf('man %d is engaged to girl %d\n', theman, thegirl);
+                freemen(theman,2) = 0;%man is not free anymore
+            else %"thegirl" is already engaged ==> check if "thegirl" prefers "theman" to her "fiance"
+                fiance = engaged(index,1);%fiance of first choice
+                 girlprefers = f(thegirl,:);
+                howgirllikestheman=find(girlprefers==theman,1); %"theman"'s number on "thegirl" preferences list
+                howgirllikesfiance=find(girlprefers==fiance,1); %"fiance"'s number on "thegirl" preferences list
+                if(isempty(howgirllikestheman)) %"thegirl" doesn't know "theman" ==> "thegirl" choose beetwen "theman" and her "fiance" (the choice is random, with a bigger chance for the fiance)
+                    if rand > 0.75
+                        %"thegirl" prefers "theman" ==> actualize
+                        %preference list of "the girl"
+                        f(thegirl,:) = [f(thegirl,1:howgirllikesfiance), theman, f(thegirl,howgirllikesfiance+1:n-1)];
+                    end %if_2
+                end %if_1
+                if(find(girlprefers==theman,1)<find(girlprefers==fiance,1)) %"thegirl" prefers "theman" ==> change engagement
+                    engaged(theman,1) = theman;%change fiance of the girl
+                    engaged(theman,2) = thegirl;
+                    engaged(fiance,1) = 0;%fiance is free again
+                    engaged(fiance,2) = 0;
+                    fprintf('girl %d dumped man %d for man %d\n', thegirl, fiance, theman);
+                    dumped=dumped+1;
+                    freemen(theman,2) = 0;
+                    freemen(fiance,2) = 1;
+                else
+                    m(theman,:) = [m(theman,2:n) 0];%"thegirl" prefers her fiance ==> take "thegirl" out of "theman"'s preference list
+                end %if_3
+            end %if_2
+        end %if_1
+end %while
+
+if dumped==1
+    fprintf('\n%d man has been dumped for another\n\n', dumped);
+else
+    fprintf('\n%d men have been dumped for others\n\n', dumped);
+end %if
+single = size(find(engaged(:,2)==0),1);   %number of single men/women
+if single==1
+    fprintf('There is %d single man/woman\n\n', single);
+else
+    fprintf('There are %d single men/women\n\n', single);
+end %if
+[stable, counter] = checkEngagements2(engaged,m,f);%check the engagements
 if (stable)
     disp('marriages are stable');
 else
-    disp('marriages are unstable');
+    fprintf('marriages are unstable\n');
+    if counter==1
+        fprintf('there is %d unstable mariage', counter);
+    else
+       fprintf('there are %d unstable mariages', counter); 
+    end %if
 end
-
