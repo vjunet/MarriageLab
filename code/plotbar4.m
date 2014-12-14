9ea65c94f64a6d188f498cc8e0e7fcfdebfdd2a2
vjunet <vjunet@student.ethz.ch>
2014-12-14 18:42:08 +0100

    Plot, data

--- code/plotbar4.m
+++ code/plotbar4.m
@@ -9,13 +9,13 @@ tmax=11;
 t = 2.^(1:tmax);
 for i=1:tmax
     dataplot(i,5,2)=dataplot(i,5,2)/t(i);
-    xlabel('input size 10^x');
-    ylabel('# singles/size');
-    title(sprintf('No of singles divided by the corresponding size for random radius'));
 end
 hold on
-handle=figure(22);
+handle=figure(1);
 bar(1:tmax,dataplot(:,5,2));
+xlabel('input size 10^x');
+ylabel('# singles/size');
+title(sprintf('No of singles divided by the corresponding size for random radius'));
 hold off
 if saveit==1
     saveas(handle,sprintf('%s/No of singles divided by the corresponding size for random radius.pdf', dirname));
